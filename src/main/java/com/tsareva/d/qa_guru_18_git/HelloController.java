dc383ba06c97d9f93f1204fd9e3a5832b94348e7
tsar-dasha <dashatsarouva@gmail.com>
2024-07-01 23:36:34 +0200

    Update HelloController.java

--- src/main/java/com/tsareva/d/qa_guru_18_git/HelloController.java
+++ src/main/java/com/tsareva/d/qa_guru_18_git/HelloController.java
@@ -6,7 +6,20 @@ import javafx.scene.control.Label;
 public class HelloController {
     @FXML
     private Label welcomeText;
-
+    fx:controller="com.tsareva.d.qa_guru_18_git.HelloController">
+    <padding>
+        fx:controller="com.tsareva.d.qa_guru_18_git.HelloController">
+    <padding>
+        fx:controller="com.tsareva.d.qa_guru_18_git.HelloController">
+    <padding>
+        fx:controller="com.tsareva.d.qa_guru_18_git.HelloController">
+    <padding>
+        fx:controller="com.tsareva.d.qa_guru_18_git.HelloController">
+    <padding>
+        fx:controller="com.tsareva.d.qa_guru_18_git.HelloController">
+    <padding>
+        fx:controller="com.tsareva.d.qa_guru_18_git.HelloController">
+    <padding>
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
